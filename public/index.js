78353bd8771fd475dea85b1774c112bd64324711
EdwinHigueraA <ehigueraa@ucentral.edu.co>
2023-11-08 12:24:46 -0500

    Cambio en el mensaje de carga

--- public/index.js
+++ public/index.js
@@ -1,14 +1,19 @@
 const button = document.querySelector('button');
 const respuesta = document.querySelector('#respuesta');
+const loadingIndicator = document.getElementById('loading');
 
 button.addEventListener('click', (e) => {
-    e.preventDefault(); // Evita que el botón realice una acción por defecto
+    e.preventDefault();
 
     // Verificar que el campo de texto no esté vacío
     if (respuesta.value.trim() === '') {
         alert('Por favor, escribe un comentario antes de enviar.');
         return; // Detiene la función aquí si el campo está vacío
     }
+
+    // Deshabilitar el botón y mostrar el indicador de carga
+    button.disabled = true;
+    loadingIndicator.style.display = 'block';
 
     const rest = respuesta.value;
 
@@ -19,16 +24,26 @@ button.addEventListener('click', (e) => {
         },
         body: JSON.stringify({ rest }),
     })
-    .then(response => response.json())
+    .then(response => {
+        // Verificar que la respuesta está bien antes de convertir a JSON
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
     .then(data => {
-        // Limpiar el campo de texto después del envío
+        // Limpiar el campo de texto y redirigir
         respuesta.value = '';
-        
-        // Redirigir a la página de agradecimiento
         window.location.href = '/thanks.html';
     })
     .catch((err) => {
-        // Manejo de errores aquí, si es necesario
+        // Manejo de errores aquí
         console.error('Error al enviar los datos:', err);
+        alert('Hubo un problema al enviar tu comentario. Por favor, inténtalo de nuevo.');
+    })
+    .finally(() => {
+        // Reactivar el botón y ocultar el indicador de carga
+        button.disabled = false;
+        loadingIndicator.style.display = 'none';
     });
 });
