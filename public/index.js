86f81bb36e41fb133b0e55e0a2ed7884c9046cba
EdwinHigueraA <ehigueraa@ucentral.edu.co>
2023-11-08 12:01:01 -0500

    Esilos y thaks page

--- public/index.js
+++ public/index.js
@@ -1,19 +1,25 @@
+const button = document.querySelector('button');
+const respuesta = document.querySelector('#respuesta');
 
-const respuesta = document.querySelector('#respuesta')
+button.addEventListener('click', (e) => {
+    e.preventDefault(); // Evita que el botón realice una acción por defecto
 
+    const rest = respuesta.value;
 
-const button = document.querySelector('button')
-
-button.addEventListener('click',(e)=> {
-    const rest = respuesta.value
-
-    fetch('/api/v1/clima',{
+    fetch('/api/v1/clima', {
         method: 'POST',
-        headers:{
-        'Content-Type':'application/json',
+        headers: {
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-            rest
-        }),
+        body: JSON.stringify({ rest }),
     })
-})
+    .then(response => response.json())
+    .then(data => {
+        // Si todo está bien, redirecciona a la página de agradecimiento
+        window.location.href = '/thanks.html';
+    })
+    .catch((err) => {
+        // Manejo de errores aquí, si es necesario
+        console.error('Error al enviar los datos:', err);
+    });
+});
