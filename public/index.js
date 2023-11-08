2d76aabd7d61a26f7725d57d30cb95c53abff295
EdwinHigueraA <ehigueraa@ucentral.edu.co>
2023-11-08 12:18:48 -0500

    Control de campos en el formulario

--- public/index.js
+++ public/index.js
@@ -4,6 +4,12 @@ const respuesta = document.querySelector('#respuesta');
 button.addEventListener('click', (e) => {
     e.preventDefault(); // Evita que el botón realice una acción por defecto
 
+    // Verificar que el campo de texto no esté vacío
+    if (respuesta.value.trim() === '') {
+        alert('Por favor, escribe un comentario antes de enviar.');
+        return; // Detiene la función aquí si el campo está vacío
+    }
+
     const rest = respuesta.value;
 
     fetch('/api/v1/clima', {
@@ -15,7 +21,10 @@ button.addEventListener('click', (e) => {
     })
     .then(response => response.json())
     .then(data => {
-        // Si todo está bien, redirecciona a la página de agradecimiento
+        // Limpiar el campo de texto después del envío
+        respuesta.value = '';
+        
+        // Redirigir a la página de agradecimiento
         window.location.href = '/thanks.html';
     })
     .catch((err) => {
